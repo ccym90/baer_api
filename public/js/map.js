15206a42775bb7a85c78d4cb137138a391f86bc6
[ccym90] <[email address]>
2017-03-15 16:57:36 +0800

    map problems

---
+++ public/js/map.js
@@ -0,0 +1,75 @@
+var map = null;
+
+function geocodeAddress( geocoder, map){
+
+  var address = $('#location').val();
+  geocoder.geocode({'address': address}, function(results, status){
+
+    if(status === 'OK'){
+
+      map.setCenter(results[0].geometry.location);
+
+      var marker = new google.maps.Marker({
+        position: results[0].geometry.location,
+        map: map,
+        animation: google.maps.Animation.DROP
+      });
+
+    }else{
+      console.log(status);
+    }
+  });
+}
+
+var option= {
+  enableHighAccuracy: true,
+  time: 5000,
+  maximumAge: 0
+};
+
+function error(err){
+  console.log("err", err);
+}
+
+function success(pos){
+  var latLng = pos.coords;
+
+  map.setCenter({lat: latLng.latitude, lng: latLng.longitude});
+  map.setZoom(3);
+
+  var marker = new google.maps.Marker({
+    position: {lat: latLng.latitude, lng: latLng.longitude},
+    map: map,
+    animation: google.maps.Animation.DROP
+  });
+
+  var geocoder = new google.maps.Geocoder();
+
+  $('#geocode').click(function(e){
+    e.preventDefault();
+    geocodeAddress(geocoder, map);
+  });
+  console.log(latLng);
+}
+
+function getLocation(){
+  navigator.geolocation.getCurrentPosition(success, error, option);
+}
+
+function initMap() {
+
+      var uluru = {lat: -25.363, lng: 131.044};
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: uluru
+        });
+
+        console.log("initMap()");
+        getLocation();
+
+}
+
+
+// $(function() {
+//   console.log("jQuery document ready");
+// });
---
+++ public/js/shop.js
@@ -0,0 +1,32 @@
+// 
+
+
+
+//add item into basket > adds into cart
+
+function (add) {
+
+$('#shop').on('click', 'button.add', function(event){
+	console.log("add button clicked")
+
+
+})
+
+}
+
+// checkout sotres location in database 
+
+$('#cart').on('click', 'button.checkout', function(event){
+	console.log("Checkout button clicked")
+})
+
+
+// pulls data onto map
+
+
+
+
+//total cost 
+
+
+
